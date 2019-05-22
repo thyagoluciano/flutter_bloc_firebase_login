e89edba401e3420418d30da24928a70b62473104
thyagoluciano <thyagoluciano@gmail.com>
2019-05-21 22:35:42 -0300

    migrate to androidX

---
+++ lib/ui/login/login_screen.dart
@@ -0,0 +1,49 @@
+import 'package:firebase_login/bloc/login/login.dart';
+import 'package:firebase_login/repository/user_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'login.dart';
+
+
+class LoginScreen extends StatefulWidget {
+  final UserRepository _userRepository;
+
+  LoginScreen({Key key, @required UserRepository userRepository})
+      : assert(userRepository != null),
+        _userRepository = userRepository,
+        super(key: key);
+
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  LoginBloc _loginBloc;
+
+  UserRepository get _userRepository => widget._userRepository;
+
+  @override
+  void initState() {
+    super.initState();
+    _loginBloc = LoginBloc(
+      userRepository: _userRepository,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Login')),
+      body: BlocProvider<LoginBloc>(
+        bloc: _loginBloc,
+        child: LoginForm(userRepository: _userRepository),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _loginBloc.dispose();
+    super.dispose();
+  }
+}
